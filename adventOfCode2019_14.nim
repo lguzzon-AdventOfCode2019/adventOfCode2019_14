7a894919f0fa3a1544db4fe7bc31e3cd3654b3f2
Luca Guzzon <luca.guzzon@gmail.com>
2019-12-18 00:40:06 +0100

    First try ...

--- adventOfCode2019_14.nim
+++ adventOfCode2019_14.nim
@@ -4,7 +4,63 @@ import sequtils
 import tables
 
 const
-  gcInput = """2 RWPCH => 9 PVTL
+  gcInputTest00 = """10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL"""
+
+  gcInputTest01 = """9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL"""
+
+  gcInputTest02 = """157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"""
+
+  gcInputTest03 ="""2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF"""
+
+  gcInputTest04="""171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX"""
+
+  gcInputOk = """2 RWPCH => 9 PVTL
 1 FHFH => 4 BLPJK
 146 ORE => 5 VJNBT
 8 KDFNZ, 1 ZJGH, 1 GSCG => 5 LKPQG
@@ -63,6 +119,10 @@ const
 13 ZSCX, 4 XLFZJ => 8 LMNCH
 1 RPWKC, 1 FDSX, 2 BJVQM => 8 JFKGV
 1 WJDF, 1 LKPQG => 4 XSNKB"""
+  # gcInput = gcInputTest00 # --> 31
+  # gcInput = gcInputTest01 # --> 165
+  # gcInput = gcInputTest02 # --> 13312
+  gcInput = gcInputTest03 # --> 13312
 
   gcLines = gcInput.split('\n')
 
@@ -80,12 +140,14 @@ const
 
 # if "12" =~ peg("{\\d+}"):
 #   echo matches[0]
-
-var lTable: Table[string, string]
+type
+  Eq = tuple[m: BiggestInt, v: seq[tuple[m: BiggestInt, k: string]]]
+var lTable: Table[string, Eq]
 for lLine in gcLines:
   let lEq = lLine.split(" => ")
   let lVal = lEq[1].split(' ')
-  lTable[lVal[1]] = lVal[0] & " => " & lEq[0]
+  lTable[lVal[1]] = (lVal[0].parseBiggestInt, lEq[0].split(", ").mapIt((
+      it.split(" ")[0].parseBiggestInt, it.split(" ")[1])))
 echo lTable
 
 var lKeys = toSeq(lTable.keys)
@@ -96,36 +158,66 @@ while lTable.len > lToSave:
     lKeys.insert(lKey, 0)
     lToSave += 1
   else:
-    var lVal: string
-    if lTable.take(lKey, lVal):
-      echo lKey, " -> ", lVal
-      let lVals = lVal.split(" => ")
-      let lMul = lVals[0].parseBiggestInt
-      for lK in lKeys:
-        let lV = lTable[lK]
-        if lV.contains(lKey):
-          echo lK, " -->> ", lV
-          var lNewVSeq: seq[string]
-          let lVSplit = lV.split(" => ")
-          for ls in lVSplit[1].split(", "):
-            if ls.contains(lKey):
-              let lsSplit = ls.split(' ')
-              let lsSplitVal = lsSplit[0].parseBiggestInt
-              var newMul = lsSplitVal div lMul
-              if lsSplitVal mod lMul != 0:
+    var lVal = lTable[lKey]
+    if lVal.v.allIt(it.k == "ORE"):
+      lKeys.insert(lKey, 0)
+      lToSave += 1
+    else:
+      var lVal: Eq
+      if lTable.take(lKey, lVal):
+        echo lKey, " -> ", lVal
+        for lK in lKeys:
+          let lV = lTable[lK].v
+          echo "--> ", lV
+          var lNewT: Table[string, BiggestInt]
+          for v in lV:
+            if v.k == lKey:
+              var newMul = v.m div lVal.m
+              if v.m mod lVal.m != 0:
                 newMul += 1
-              var lNewVal: seq[string]
-              for lValsItem in lVals[1].split(", "):
-                let lValsItems = lValsItem.split(' ')
-                lNewVal.add($(lValsItems[0].parseBiggestInt * newMul) &
-                            " " &
-                            lValsItems[1])
-              lNewVSeq.add(lNewVal.join(", "))
+              for lTo in lVal.v:
+                let lNewM = newMul * lTo.m
+                if lNewT.hasKeyOrPut(lTo.k, lNewM):
+                  lNewT[lTo.k] += lNewM
             else:
-              lNewVSeq.add(ls)
-          let lNewV = lVSplit[0] & " => " & lNewVSeq.join(", ")
-          echo lK, " -->> ", lNewV
-          lTable[lk] = lNewV
+              if lNewT.hasKeyOrPut(v.k, v.m):
+                lNewT[v.k] += v.m
+          let lNewV = toSeq(lNewT.pairs).mapIt((it[1], it[0]))
+          echo "<-- ", lNewV
+          lTable[lK].v = lNewV
 
 echo lTable
-echo lTable["FUEL"].split(", ").foldl(a + (b.split(" ")[0].parseBiggestInt), 0i64)
+
+lKeys = toSeq(lTable.keys)
+lToSave = 0
+while lTable.len > lToSave:
+  let lKey = lKeys.pop
+  if lKey == "FUEL":
+    lKeys.insert(lKey, 0)
+    lToSave += 1
+  else:
+      var lVal: Eq
+      if lTable.take(lKey, lVal):
+        echo lKey, " -> ", lVal
+        for lK in lKeys:
+          let lV = lTable[lK].v
+          echo "--> ", lV
+          var lNewT: Table[string, BiggestInt]
+          for v in lV:
+            if v.k == lKey:
+              var newMul = v.m div lVal.m
+              if v.m mod lVal.m != 0:
+                newMul += 1
+              for lTo in lVal.v:
+                let lNewM = newMul * lTo.m
+                if lNewT.hasKeyOrPut(lTo.k, lNewM):
+                  lNewT[lTo.k] += lNewM
+            else:
+              if lNewT.hasKeyOrPut(v.k, v.m):
+                lNewT[v.k] += v.m
+          let lNewV = toSeq(lNewT.pairs).mapIt((it[1], it[0]))
+          echo "<-- ", lNewV
+          lTable[lK].v = lNewV
+
+echo lTable
+echo lTable["FUEL"].v.foldl(a + b.m, 0i64)
